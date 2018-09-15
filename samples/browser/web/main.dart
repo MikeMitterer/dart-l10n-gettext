cb0734c4e3b51fc43bf5beb816177155ec02e320
Mike Mitterer <office@mikemitterer.at>
2018-09-15 10:29:10 +0200

    doc: Browser-Sample got a separate _initLanguageSettings-function

--- samples/browser/web/main.dart
+++ samples/browser/web/main.dart
@@ -9,6 +9,25 @@ import 'package:l10n/l10n.dart';
 import 'package:browser_example/_l10n/messages_all.dart';
 
 Future main() async {
+    final String locale = await _initLanguageSettings();
+
+    (dom.querySelector("head") as dom.HeadElement).lang = locale;
+
+    dom.querySelectorAll('.translate')
+        .map((final dom.Element element) => element as dom.HtmlElement)
+        .forEach((final dom.HtmlElement element) {
+            element.text = element.text.replaceAllMapped(RegExp(r'_\("(.*)"\)', multiLine: true),
+                    (final Match match) {
+                    return l10n(match.group(1));
+                });
+    });
+
+    dom.querySelector("body").classes.remove("loading");
+}
+
+/// Initialize the Intl-Framework
+/// 
+Future<String> _initLanguageSettings() async {
     // Determine your locale
     final String locale = await findSystemLocale();
     final String shortLocale = Intl.shortLocale(Uri.base.queryParameters['lang'] ?? locale);
@@ -24,16 +43,5 @@ Future main() async {
     // Initialize translation-table
     await initializeMessages(shortLocale);
 
-    (dom.querySelector("head") as dom.HeadElement).lang = shortLocale;
-
-    dom.querySelectorAll('.translate')
-        .map((final dom.Element element) => element as dom.HtmlElement)
-        .forEach((final dom.HtmlElement element) {
-            element.text = element.text.replaceAllMapped(RegExp(r'_\("(.*)"\)', multiLine: true),
-                    (final Match match) {
-                    return l10n(match.group(1));
-                });
-    });
-
-    dom.querySelector("body").classes.remove("loading");
+    return shortLocale;
 }
