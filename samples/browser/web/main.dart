d30b7879ff7b2653fd8426ff5fa15f8622549e51
Vohmyanin Sergey Vasilevich <delagen@gmail.com>
2020-04-02 11:20:40 +0300

    fix compatibility with latest dart SDK

--- samples/browser/web/main.dart
+++ samples/browser/web/main.dart
@@ -9,7 +9,7 @@ import 'package:browser_example/_l10n/messages_all.dart';
 
 Future main() async {
     // initLanguageSettings checks the browser url if it finds
-    // a "lang" query param and sets the locale accordingly 
+    // a "lang" query param and sets the locale accordingly
     final String locale = await initLanguageSettings(
             () => findSystemLocale(),
             (final String locale) => initializeMessages(locale)
